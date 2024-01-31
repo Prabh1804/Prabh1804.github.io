29a235932fe76ad26892d123ea47d97e096bc6f0
Prabhdeep singh <prabhdeep5singh555@gmail.com>
2024-02-01 01:18:17 +0530

    added first project only in desktop region

---
+++ script.js
@@ -0,0 +1,9 @@
+function go_to_about(){
+	console.log("about is clicked");
+}
+function go_to_project(){
+	console.log("about is project");
+}
+function go_to_contact(){
+	console.log("about is contact");
+} 
