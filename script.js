a904999b90499fb82f4d92a6351b237083cc1cce
Prabhdeep singh <prabhdeep5singh555@gmail.com>
2024-03-19 17:06:59 +0530

    dark and bright theme added

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+sunFunc = () => {
+	let sunButton = document.querySelector(".sun-button");
+	sunButton.style.display = "none";
+	document.documentElement.style.setProperty('--hue-value1', '0')
+	document.querySelector(".moon-button").style.display = "block" ;
+};
+moonFunc = () => {
+	let moonButton = document.querySelector(".moon-button");
+	moonButton.style.display = "none";
+	document.documentElement.style.setProperty('--hue-value1', '100%')
+	document.querySelector(".sun-button").style.display = "block" ;
+};
+
 function go_to_about(){
 	console.log("about is clicked");
 }
